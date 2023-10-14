14b014350afca3d8d16d9820aecbc88025a8a5d2
Sshivam26 <shivamam26@gmail.com>
2023-10-14 19:53:07 +0530

    commit 2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// state
+
 let currCity = "London";
 let units = "metric";
 
-// Selectors
+
 let city = document.querySelector(".weather__city");
 let datetime = document.querySelector(".weather__datetime");
 let weather__forecast = document.querySelector('.weather__forecast');
@@ -14,40 +14,38 @@ let weather__humidity = document.querySelector('.weather__humidity');
 let weather__wind = document.querySelector('.weather__wind');
 let weather__pressure = document.querySelector('.weather__pressure');
 
-// search
+
 document.querySelector(".weather__search").addEventListener('submit', e => {
     let search = document.querySelector(".weather__searchform");
-    // prevent default action
+
     e.preventDefault();
-    // change current city
+
     currCity = search.value;
-    // get weather forecast 
+ 
     getWeather();
-    // clear form
+ 
     search.value = ""
 })
 
-// units
+
 document.querySelector(".weather_unit_celsius").addEventListener('click', () => {
     if(units !== "metric"){
-        // change to metric
+        
         units = "metric"
-        // get weather forecast 
+
         getWeather()
     }
 })
 
 document.querySelector(".weather_unit_farenheit").addEventListener('click', () => {
     if(units !== "imperial"){
-        // change to imperial
         units = "imperial"
-        // get weather forecast 
         getWeather()
     }
 })
 
 function convertTimeStamp(timestamp, timezone){
-     const convertTimezone = timezone / 3600; // convert seconds to hours 
+     const convertTimezone = timezone / 3600; 
 
     const date = new Date(timestamp * 1000);
     
@@ -58,16 +56,13 @@ function convertTimeStamp(timestamp, timezone){
         year: "numeric",
         hour: "numeric",
         minute: "numeric",
-        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+        timeZone: `Asia/Kolkata`,
         hour12: true,
     }
-    return date.toLocaleString("en-US", options)
+    return date.toLocaleString("en-IN", options)
    
 }
 
- 
-
-// convert country code to name
 function convertCountryCode(country){
     let regionNames = new Intl.DisplayNames(["en"], {type: "region"});
     return regionNames.of(country)
@@ -76,19 +71,46 @@ function convertCountryCode(country){
 function getWeather(){
     const API_KEY = 'a06cddfa5823c21fbb96dc9995ad5cc7'
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => {
-    console.log(data)
-    city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
-    datetime.innerHTML = convertTimeStamp(data.dt, data.timezone); 
-    weather__forecast.innerHTML = `<p>${data.weather[0].main}`
-    weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
-    weather__icon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
-    weather__minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
-    weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
-    weather__humidity.innerHTML = `${data.main.humidity}%`
-    weather__wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
-    weather__pressure.innerHTML = `${data.main.pressure} hPa`
-})
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => {
+        console.log(data)
+        city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
+        datetime.innerHTML = convertTimeStamp(data.dt, data.timezone); 
+        weather__forecast.innerHTML = `<p>${data.weather[0].main}`
+        weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+        weather__icon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
+        weather__minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
+        weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+        weather__humidity.innerHTML = `${data.main.humidity}%`
+        weather__wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
+        weather__pressure.innerHTML = `${data.main.pressure} hPa`
+        const body = document.body; 
+            const weatherCondition = data.weather[0].main;
+
+            switch (weatherCondition) {
+                case "Haze":
+                    body.style.backgroundImage = "url('/image/haze.jpg')";
+                    break;
+                case "clear sky":
+                    body.style.backgroundImage = "url('/image/clear-sky.jpg')";
+                    break;
+                case "Rain":
+                    body.style.backgroundImage = "url('/image/rain.jpg')";
+                    break;
+                case "Mist":
+                    body.style.backgroundImage = "url('/image/mist.jpg')";
+                    break;
+                case "Clouds":
+                    body.style.backgroundImage = "url('/image/clouds.jpg')";
+                    break;
+                case "Snow":
+                    body.style.backgroundImage = "url('/image/snow.jpg')";
+                    break;
+                default:
+                    body.style.backgroundImage = "url('/image/default.jpg')";
+                    break;
+            }
+    })
+    
 }
 
 document.body.addEventListener('load', getWeather())
